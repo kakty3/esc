2772c77657b470f2f43ecb9e6ba87a8ad8bfe7a1
Sergey Demurin <kakty3.mail@gmail.com>
2017-06-03 16:53:31 +0700

    added `normalize` option to author attributes distance functions

--- esc_identifier/esc_identifier/author.py
+++ esc_identifier/esc_identifier/author.py
@@ -10,24 +10,26 @@ Author = namedtuple('Author', ['kdd_id', 'name', 'affiliation'])
 RealAuthor = namedtuple('RealAuthor', ['kdd_author_ids', 'name', 'affiliation'])
 
 
-def human_name_distance(a: str, b: str):
+def human_name_distance(a: str, b: str, normalize=False):
     distance = token_set_distance
-    a_normalized, b_normalized = \
-        normalize_human_name(a), normalize_human_name(b)
-    return distance(a_normalized, b_normalized)
+    return distance(
+        normalize_human_name(a) if normalize else a,
+        normalize_human_name(b) if normalize else b,
+    )
 
 
 # TODO: detect countries and cities
-def affiliation_distance(a: str, b: str):
+def affiliation_distance(a: str, b: str, normalize=False):
     distance = token_set_distance
-    a_normalized, b_normalized = \
-        normalize_affiliation(a), normalize_affiliation(b)
-    return distance(a_normalized, b_normalized)
+    return distance(
+        normalize_affiliation(a) if normalize else a,
+        normalize_affiliation(b) if normalize else b,
+    )
 
 
-def author_distance(a: Author, b: Author):
+def author_distance(a: Author, b: Author, normalize=False):
     distances_vector = [
-        human_name_distance(a.name, b.name),
-        affiliation_distance(a.affiliation, b.affiliation)
+        human_name_distance(a.name, b.name, normalize),
+        affiliation_distance(a.affiliation, b.affiliation, normalize)
     ]
     return distances_vector
