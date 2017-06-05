734d2e07dcf95947c57b5f7011799f1929f47a42
Sergey Demurin <kakty3.mail@gmail.com>
2017-06-05 22:53:48 +0700

    added model for table `author`

--- esc_identifier/esc_identifier/db/model/kdd/__init__.py
+++ esc_identifier/esc_identifier/db/model/kdd/__init__.py
@@ -1,10 +1,14 @@
 from sqlalchemy import (
-    Column, Integer, String,
+    Column, Integer, String
 )
-from sqlalchemy.schema import PrimaryKeyConstraint
+from sqlalchemy.schema import (
+    PrimaryKeyConstraint, ForeignKey
+)
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 from esc_identifier.db.utils import db_model_repr
+
 
 Base = declarative_base()
 Base.__repr__ = db_model_repr
@@ -21,11 +25,23 @@ class Paper(Base):
     keyword = Column(String)
 
 
+class Author(Base):
+    __tablename__ = 'author'
+
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String)
+    affiliation = Column(String)
+
+
 class PaperAuthor(Base):
     __tablename__ = 'paperauthor'
 
-    paperid = Column(Integer)
-    authorid = Column(Integer)
+    paperid = Column(Integer, ForeignKey('paper.id'))
+    paper = relationship("Paper", backref="paperauthor")
+
+    authorid = Column(Integer, ForeignKey('author.id'))
+    author = relationship("Author", backref="paperauthor")
+
     name = Column(String)
     affiliation = Column(String)
 
